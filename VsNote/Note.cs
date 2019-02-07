1d2e3e0a4ce82ec9d8127c79f57330686240fdbf
Esmaeil Mazahery <smaeil.mazahery@gmail.com>
2019-02-07 12:44:44 +0330

    addproject

---
+++ VsNote/Note.cs
@@ -0,0 +1,34 @@
+﻿namespace VsNote
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("bc643cd3-1df3-4a54-bf63-56fb5000daad")]
+    public class Note : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Note"/> class.
+        /// </summary>
+        public Note() : base(null)
+        {
+            this.Caption = "Note";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new NoteControl();
+        }
+    }
+}
---
+++ VsNote/NoteControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿namespace VsNote
+{
+    using Microsoft.VisualStudio.Shell;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Interaction logic for NoteControl.
+    /// </summary>
+    /// 
+    public partial class NoteControl : UserControl
+    {
+        private ObservableCollection<NoteItem> timeStepDataCollection;
+        public ObservableCollection<NoteItem> TimeStepDataCollection
+        {
+            get
+            {
+                if (timeStepDataCollection == null)
+                {
+                    timeStepDataCollection = new ObservableCollection<NoteItem>();
+                    timeStepDataCollection.Add(new NoteItem() { Title = "test", brush = Brushes.White });
+                }
+                return timeStepDataCollection;
+            }
+            set
+            {
+                timeStepDataCollection = value;
+            }
+        }
+
+        public NoteControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public class NoteItem
+        {
+            public string Title { get; set; }
+            public Brush brush { set; get; }
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            noteList.Items.Add(textBox.Text);
+        }
+
+        private void noteList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (noteList.SelectedIndex != -1)
+            {
+                Clipboard.SetText(noteList.SelectedValue.ToString());
+                textBox.Text = noteList.SelectedValue.ToString();
+                noteList.SelectedIndex = -1;
+            }
+        }
+
+        private void textBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+                noteList.Items.Add(textBox.Text);
+        }
+
+        private void btn_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var i in noteList.Items)
+            {
+                if (i.ToString() == textBox.Text)
+                {
+                    noteList.Items.Remove(i);
+                    break;
+                }
+            }
+        }
+    }
+}
